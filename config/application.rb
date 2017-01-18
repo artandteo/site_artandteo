8a757924f0ec5273a59aa9d4dfb870d9a4551db7
Fred Reuby <frederic.reubrecht@gmail.com>
2017-01-18 18:12:41 +0100

    ajout asset ckeditor

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,9 @@ module ArtandteoSite
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    config.assets.precompile += Ckeditor.assets
+    config.assets.precompile += %w( ckeditor/* )
     config.autoload_paths += %w(#{config.root}/app/models/ckeditor)
-    
+
   end
 end
