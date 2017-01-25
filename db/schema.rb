42bc5587ebbee3dbe6794920dd0f10d6aa3caa12
Fred Reuby <frederic.reubrecht@gmail.com>
2017-01-25 17:18:21 +0100

    page agence

--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20170123134752) do
 
-  create_table "blog_posts", force: :cascade do |t|
-    t.string   "title"
-    t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "blogs", force: :cascade do |t|
     t.string   "titre"
     t.text     "contenu"
@@ -43,8 +36,6 @@ ActiveRecord::Schema.define(version: 20170123134752) do
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
     t.string   "encrypted_password",     default: "",    null: false
-    t.string   "firstname",              default: "",    null: false
-    t.string   "lastname",               default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -53,6 +44,10 @@ ActiveRecord::Schema.define(version: 20170123134752) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.boolean  "is_admin",               default: false
