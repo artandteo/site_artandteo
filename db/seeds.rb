1965378a78d7d368151bf4e1f6d9190a797a434a
Fred Reuby <frederic.reubrecht@gmail.com>
2017-01-27 17:52:43 +0100

    modif seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(firstname: "John", lastname: "Doe", email: "admin@artandteo.com", password: "admin0", password_confirmation: "admin0", is_admin: true)
+User.create(lastname: "Doe", email: "admin@artandteo.com", password: "admin0", password_confirmation: "admin0", is_admin: true)
