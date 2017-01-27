5c4b18212ef435cb6bb7bb1c16a2cac244dd6c3a
Fred Reuby <frederic.reubrecht@gmail.com>
2017-01-27 09:17:04 +0100

    design pages users

--- db/migrate/20170123134752_devise_create_users.rb
+++ db/migrate/20170123134752_devise_create_users.rb
@@ -4,7 +4,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-
       t.string :firstname,          null: false, default: ""
       t.string :lastname,           null: false, default: ""
 
@@ -37,7 +36,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
       t.timestamps null: false
 
       t.boolean :is_admin,                   default: false
-      
+
     end
 
     add_index :users, :email,                unique: true
