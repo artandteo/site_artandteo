f9082a4a4ecead3fb1b25e189b5c747d618cd081
Kévin Cadieu <kevin.cadieu@gmail.com>
2017-02-28 14:03:29 +0100

    blog

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -11,15 +11,7 @@ class Blog < ApplicationRecord
 	belongs_to :categorie
 	belongs_to :user, required: false, foreign_key: "users_id"
 
-	def next
-    	Blog.where("id > ?", @blog).first
-  	end
-
-  	def prev
-    	Blog.where("id < ?", params[:id]).last
-  	end
-
-    private 
+  private 
 
   	def default_slug
   		if !self.titre.empty? && self.slug.empty?
