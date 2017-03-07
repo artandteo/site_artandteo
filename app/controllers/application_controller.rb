666254a91372b97e5e072cd927987d9a5d9eb8cc
Fred Reuby <frederic.reubrecht@gmail.com>
2017-03-07 15:03:10 +0100

    ajouts pour google analytics et meta

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < ActionController::Base
   end
 
   def default_url_options(options={})
+    { host: ENV['HOST'] || 'localhost:3000' }
     { :locale => I18n.locale == I18n.default_locale ? nil : I18n.locale  }
   end
 end
